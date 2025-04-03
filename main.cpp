5a92a58a3c16efe2f5e159889120126f52688827
Lolefin23 <leonardodelboni3@gmail.com>
2025-04-03 10:40:05 -0400

    Incia a parte de renderização

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace QuickCG;
 #define telaAltura 640
 #define telaLargura 480
 
-int worldMap[larguraMapa][alturaMapa] =
+int mapaMundo[larguraMapa][alturaMapa] =
     {
         {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
         {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -91,8 +91,50 @@ int main()
                 ladoDistX = (mapaX + 1 - posX) * deltaDistX;
             }
 
-            if (raioDirX > 0)
+            if (raioDirY < 0)
             {
+                passoY = -1;
+                ladoDistY = (posY - mapaY) * ladoDistY;
+            }
+            else
+            {
+                passoY = 1;
+                ladoDistY = (mapaY + 1 - posY);
+            }
+
+            // Inicia o DDA
+
+            while (acerto == 0)
+            {
+                // pula par o próximo quadrado, sendo sempre a menor distância
+                // pelo que entendi o caminho parece ser um zigue zague mas no final fica igual a uma linha reta
+
+                if (ladoDistX < ladoDistY)
+                {
+                    ladoDistX += deltaDistX;
+                    mapaX += passoX;
+                    lado = 0;
+                }
+                else
+                {
+                    ladoDistY += deltaDistY;
+                    mapaY = +passoX;
+                    lado = 1;
+                }
+
+                if (mapaMundo[mapaX][mapaY] > 0)
+                {
+
+                    // para calcular a altura da parede, não é usado as distancias reais do ponto até a parede, pois iria ficar com efeito de olho de peixe
+                    // imagine que temos uma parde reta, se usarmos a distância real, haverá distancias diferentes entre o jogador e as paredes, distorcendo a rendereização
+                    // portanto usamos a distancia do plano camera até a parede acertada
+
+                    acerto = 1;
+                    if (lado == 0)
+                        perpPardeDist = (ladoDistX - deltaDistX);
+                    else
+                        perpPardeDist = (ladoDistY - deltaDistY);
+                }
             }
         }
     }
